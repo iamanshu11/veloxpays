97bde87696d8bff49427c15235da1f901f5fe8f6
Anshuman <anshuman.raj@accropolix.live>
2025-02-03 20:04:26 +0530

    content updated with new pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,17 @@ module.exports = {
     './src/account-verification-identity.html',
     './src/account-verification-identity-passport.html',
     './src/account-verification-identity-passport-verified.html',
+    './src/account-verification-identity-driver-license.html',
+    './src/account-verification-identity-national-id.html',
+    './src/account-verification-address.html',
+    './src/account-verification-identity-telephone.html',
+    './src/account-verification-identity-gas.html',
+    './src/account-verification-identity-water.html',
+    './src/account-verification-fund.html',
+    './src/account-verification-identity-account-statement.html',
+    './src/account-verification-identity-income-tax.html',
+    './src/send-money.html',
+
 
   ],
   theme: {
